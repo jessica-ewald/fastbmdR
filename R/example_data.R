688aa40cd86360ad6dda85df35bd775d37bce06d
jessica-ewald <1jess.ewald@gmail.com>
2024-10-26 15:23:29 -0400

    add example data

---
+++ R/example_data.R
@@ -0,0 +1,11 @@
+#' Example Dataset
+#'
+#' This is an example dataset included in the `fastbmdR` package. It can be used for demonstrations and testing.
+#'
+#' @format A data frame with 263 rows (Entrez gene IDs) and 30 columns (samples)
+#' @source Randomly sampled genes from an example transcriptomics dose-response dataset.
+#' @docType data
+#' @name example_data
+#' @usage data(example_data)
+#' @keywords datasets
+NULL
